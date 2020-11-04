c18ba6c4b7392f1799212be904c3d3119ce12822
髙橋剛志 <tt2123244@gmail.com>
2020-11-04 17:11:27 +0900

    ToDo論理削除機能に変更

--- app/Todo.php
+++ app/Todo.php
@@ -3,12 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model //継承元のmodelクラスにall(),fill()などが定義されている 実行しているのはmodel
 {
+    use SoftDeletes;
+    
+    /**
+     * 日付へキャストする属性
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'title',
-        'user_id'
+        'user_id',
     ]; // fill() fillableに値が入れられるか確認してる todosテーブルの指定カラムのみcreate(),file(),update()でデータ挿入が可能になる データが勝手に書き込まれるのを防ぐ
 
     public function getByUserId($id)
