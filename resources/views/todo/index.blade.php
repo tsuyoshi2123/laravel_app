9b93aa3a0c3ed84f463359ca25a6cadb0e89c814
髙橋剛志 <tt2123244@gmail.com>
2020-11-04 13:32:51 +0900

    認証名のToDo一覧に表記変更

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.app') <!-- 親ファイルを継承する -->
 @section ('content') <!-- イールドに挿入する -->
 
-<h1 class="page-header">ToDo一覧</h1>
+<h1 class="page-header">{{ Auth::user()->name }}のToDo一覧</h1>
 <p class="text-right">
   <a class="btn btn-success" href="/todo/create">新規作成</a><!-- * aタグで送られる場合はGET -->
 </p>
